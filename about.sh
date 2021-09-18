ed349c6f6fa81844a7950e63f4554678155f5299
Daorakle <90761682+Daorakle@users.noreply.github.com>
2021-09-18 16:00:13 +0200

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,10 +2,10 @@
 
 clear
 echo -e "================================================="| lolcat
-echo -e "#         Premium Auto Script By geo            #"| lolcat
+echo -e "#         Premium Auto Script By Daora          #"| lolcat
 echo -e "#-----------------------------------------------#"| lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #"| lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"| lolcat
 echo -e "# For VPS with KVM and VMWare virtualization    #"| lolcat
-echo -e "# Build Up By geo                               #"| lolcat
+echo -e "# Build Up By Daorakle                          #"| lolcat
 echo -e "#-----------------------------------------------#"| lolcat
