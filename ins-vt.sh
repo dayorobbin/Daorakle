4e26725c02a05c509e8e7cfadb1ae7c1ab532b3f
Daorakle <90761682+Daorakle@users.noreply.github.com>
2021-09-18 16:15:54 +0200

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -58,7 +58,7 @@ cat> /etc/v2ray/config.json << END
           ]
         },
         "wsSettings": {
-          "path": "https://t.me/sampiiiiu",
+          "path": "https://t.me/daorakle",
           "headers": {
             "Host": ""
           }
@@ -146,7 +146,7 @@ cat> /etc/v2ray/none.json << END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "https://t.me/sampiiiiu",
+          "path": "https://t.me/daorakle",
           "headers": {
             "Host": ""
           }
@@ -243,7 +243,7 @@ cat> /etc/v2ray/vless.json << END
           ]
         },
         "wsSettings": {
-          "path": "https://t.me/sampiiiiu",
+          "path": "https://t.me/daorakle",
           "headers": {
             "Host": ""
           }
@@ -330,7 +330,7 @@ cat> /etc/v2ray/vnone.json << END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "https://t.me/sampiiiiu",
+          "path": "https://t.me/daorakle",
           "headers": {
             "Host": ""
           }
